4c2ad2abf6b0b4e99e04843d7d38e155dd3e9711
Kemansu <153510756+Kemansu@users.noreply.github.com>
2024-05-17 10:41:42 +0300

    Delete src/main directory

--- src/main/java/org/sc/exceptions/WrongInputException.java
+++
@@ -1,9 +0,0 @@
-package org.sc.exceptions;
-/**
- * Данное исключениет работает с неправильным пользовательским вводом
- *
- * @author Kemansu
- * @since 1.0
- */
-public class WrongInputException extends Exception{
-}
